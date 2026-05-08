b4db94d24cb0b82ac84b2995acd20c131735a796
lumiere-dev <203832244+lumiere-dev@users.noreply.github.com>
2026-05-08 16:45:21 +0530

    Add posthog snippet

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 import re
 from streamlit_cookies_controller import CookieController
 
+import streamlit.components.v1 as components
+
+# This is the "silent" tracker
+posthog_snippet = """
+<script>
+    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}var l=t;for(void 0!==a?l=t[a]=[]:a="posthog",l.people=l.people||[],l.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},l.people.toString=function(){return l.toString(1)+".people (stub)"},p="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures onSessionId getSurveys getActiveMatchingSurveys renderSurvey getNextSurveyStep identifyAndSetDistinctId".split(" "),r=0;r<p.length;r++)g(l,p[r]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('phc_oVPvA5P5XDgN389qFX9pUrZykk7ykhRHLJUJ6PKHNMFF', {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only' 
+    })
+</script>
+"""
+
+# Inject the snippet into the frontend
+components.html(posthog_snippet, height=0, width=0)
+
 
 def get_secret(key, default=None):
     """Get config value from env var (Railway) or st.secrets (local), with optional default."""
